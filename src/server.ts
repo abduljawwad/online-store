e2044816859e660682b6a096c0024e8a74668440
Abdul Jawwad <abduljawwad.md@gmail.com>
2021-11-13 18:34:09 +0000

    add prettier config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 9000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world');
 });
 
 app.listen(port, () => {
