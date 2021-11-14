1657215528f836cc511f96b37b96afe3d2e6bc5e
Abdul Jawwad <abduljawwad.md@gmail.com>
2021-11-14 18:01:51 +0000

    add environment variables

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,6 +1,13 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(`db_test02`, 'postgres', 'password', {
-  dialect: 'postgres',
-});
+require('dotenv').config();
+
+const db = new Sequelize(
+  `${process.env.DB}`,
+  `${process.env.DB_USERNAME}`,
+  `${process.env.DB_PASSWORD}`,
+  {
+    dialect: 'postgres',
+  }
+);
 
 export default db;
