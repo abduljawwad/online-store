933d82d4b653a306614118a37b6ee33faaad87c9
Abdul Jawwad <abduljawwad.md@gmail.com>
2021-11-15 22:00:03 +0000

    add db.sync() for creating tables

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import db from './config/db.config';
 import {
   createProduct,
   getAllProducts,
@@ -11,6 +12,14 @@ require('dotenv').config();
 const app = express();
 const port = `${process.env.PORT}`;
 app.use(express.json());
+
+db.sync({ alter: true })
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err: string) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.use('/', createProduct);
 app.use('/', getAllProducts);
